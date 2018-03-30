c6721560373653a3236c857d059132837aaeb773
p2j2j4 <38jonesp@cua.edu>
2018-03-30 13:27:24 -0400

    Changing initial set mode from guided to stabilize before takeoff This mode is recommended by ardupilot docs before takeoff in preparation for GUIDED mode.

--- test.cpp
+++ test.cpp
@@ -16,12 +16,12 @@ int main(int argc, char **argv)
     ros::Rate r(rate);
 
     ////////////////////////////////////////////
-    /////////////////GUIDED/////////////////////
+    /////////////////STABILIZE//////////////////
     ////////////////////////////////////////////
     ros::ServiceClient cl = n.serviceClient<mavros_msgs::SetMode>("/mavros/set_mode");
     mavros_msgs::SetMode srv_setMode;
     srv_setMode.request.base_mode = 0;
-    srv_setMode.request.custom_mode = "GUIDED";
+    srv_setMode.request.custom_mode = "STABILIZE";
     if(cl.call(srv_setMode)){
         ROS_ERROR("setmode send ok %d value:", srv_setMode.response.success);
     }else{
