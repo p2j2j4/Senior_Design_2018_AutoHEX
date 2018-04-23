22a525b5d91c36fb02c30a610ad70fd8957ec6ad
Live session user <38jonesp@cua.edu>
2018-04-23 02:21:50 +0000

    White space and build error update
    ROS service in the void scanCallback caused build errors.
    New appproach will be implemented where a subscriber will publish
    velocity commands based on the proximity ov various objects.
    
    New waypoint dependencies must be added to the CMakeLists.txt and package.xml
    files in order for them to operate properly.

--- detect_avoid_files/src/test_flight.cpp
+++ detect_avoid_files/src/test_flight.cpp
@@ -14,7 +14,7 @@
 
 
 //RPLIDAR data
-#include <Sensor_msgs/LaserScan.h>
+#include <sensor_msgs/LaserScan.h>
 
 #define RAD2DEG(x) ((x)*180./M_PI)
 
@@ -32,31 +32,8 @@ void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
             if (scan->ranges[i] < .3){
                     ROS_ERROR(": [%f,%f]", degree, scan->ranges[i]);
                     ROS_INFO_STREAM(": count: "<< i);
-                    break;
              }
-
-            // Land the copter if a call to land has not already been made
-            if ((scan->ranges[i] < .3 ) && (!land_cl.call(srv_land)) ){
-
-                ////////////////////////////////////////////
-                ///////////////////LAND/////////////////////
-                ////////////////////////////////////////////
-                ros::ServiceClient land_cl = n.serviceClient<mavros_msgs::CommandTOL>("/mavros/cmd/land");
-                mavros_msgs::CommandTOL srv_land;
-                srv_land.request.altitude = 0;
-                srv_land.request.latitude = 0;
-                srv_land.request.longitude = 0;
-                srv_land.request.min_pitch = 0;
-                srv_land.request.yaw = 0;
-                if(land_cl.call(srv_land)){
-                    ROS_INFO("srv_land send ok %d", srv_land.response.success);
-                }else{
-                    ROS_ERROR("Failed Land");
-                }
-
-            }
-       }
-    
+       }   
 }
 
 int main(int argc, char **argv)
@@ -73,67 +50,61 @@ int main(int argc, char **argv)
     ////////////////////////////////////////////
     /////////////////Waypoints//////////////////
     ////////////////////////////////////////////
-  ros::ServiceClient wp_clear_client = p.serviceClient<mavros_msgs::WaypointClear>("/mavros/mission/clear"); //("waypoint_clear_client")
-  ros::ServiceClient wp_srv_client = n.serviceClient<mavros_msgs::WaypointPush>("waypoint_push_client");
-  ros::ServiceClient set_home_client = l.serviceClient<mavros_msgs::CommandHome>("command_home_client");
+    ros::ServiceClient wp_clear_client = p.serviceClient<mavros_msgs::WaypointClear>("/mavros/mission/clear"); //("waypoint_clear_client")
+    ros::ServiceClient wp_srv_client = n.serviceClient<mavros_msgs::WaypointPush>("waypoint_push_client");
+    ros::ServiceClient set_home_client = l.serviceClient<mavros_msgs::CommandHome>("command_home_client");
 
-  mavros_msgs::WaypointPush wp_push_srv;
-  mavros_msgs::WaypointClear wp_clear_srv;
-  mavros_msgs::CommandHome set_home_srv;
-
-
-  mavros_msgs::Waypoint wp_msg;
-
-  wp_msg.frame = 0; // mavros_msgs::Waypoint::FRAME_GLOBAL;
-  wp_msg.command = 16;
-  wp_msg.is_current = false;
-  wp_msg.autocontinue = false;
-  wp_msg.param1 = 0;
-  wp_msg.param2 = 0;
-  wp_msg.param3 = 0;
-  wp_msg.param4 = 0;
-  wp_msg.x_lat = 40.0;
-  wp_msg.y_long = 30.0;
-  wp_msg.z_alt = 5.0;
-
-  wp_clear_srv.request = {};
-
-  wp_push_srv.request.start_index = 0;
-  wp_push_srv.request.waypoints.push_back(wp_msg);
-
-  set_home_srv.request.current_gps = false;
-  set_home_srv.request.latitude = 11;
-  set_home_srv.request.longitude = 12;
-  set_home_srv.request.altitude = 1;
-
-  if (set_home_client.call(set_home_srv))
-{
-    ROS_INFO("Home was set to new value ");
-}
-else
-{
-    ROS_ERROR("Home position couldn't been changed");
-}
+    mavros_msgs::WaypointPush wp_push_srv;
+    mavros_msgs::WaypointClear wp_clear_srv;
+    mavros_msgs::CommandHome set_home_srv;
 
 
-if (wp_clear_client.call(wp_clear_srv))
-{
-    ROS_INFO("Waypoint list was cleared");
-}
-else
-{
-    ROS_ERROR("Waypoint list couldn't been cleared");
-}
+    mavros_msgs::Waypoint wp_msg;
 
-  if (wp_srv_client.call(wp_push_srv))
-  {
-    ROS_INFO("Success:%d", (bool)wp_push_srv.response.success);
-  }
-  else
-  {
-    ROS_ERROR("Waypoint couldn't been sent");
-    ROS_INFO("Success:%d", (bool)wp_push_srv.response.success);
-  }
+    wp_msg.frame = 0; // mavros_msgs::Waypoint::FRAME_GLOBAL;
+    wp_msg.command = 16;
+    wp_msg.is_current = false;
+    wp_msg.autocontinue = false;
+    wp_msg.param1 = 0;
+    wp_msg.param2 = 0;
+    wp_msg.param3 = 0;
+    wp_msg.param4 = 0;
+    wp_msg.x_lat = 40.0;
+    wp_msg.y_long = 30.0;
+    wp_msg.z_alt = 5.0;
+
+    wp_clear_srv.request = {};
+
+    wp_push_srv.request.start_index = 0;
+    wp_push_srv.request.waypoints.push_back(wp_msg);
+
+   set_home_srv.request.current_gps = false;
+   set_home_srv.request.latitude = 11;
+   set_home_srv.request.longitude = 12;
+   set_home_srv.request.altitude = 1;
+
+   if (set_home_client.call(set_home_srv)){
+     ROS_INFO("Home was set to new value ");
+    }
+   else{
+     ROS_ERROR("Home position couldn't been changed");
+   }
+
+
+   if (wp_clear_client.call(wp_clear_srv)){
+     ROS_INFO("Waypoint list was cleared");
+     }
+   else{
+     ROS_ERROR("Waypoint list couldn't been cleared");
+   }
+
+   if (wp_srv_client.call(wp_push_srv)){
+     ROS_INFO("Success:%d", (bool)wp_push_srv.response.success);
+   }
+   else{
+     ROS_ERROR("Waypoint couldn't been sent");
+     ROS_INFO("Success:%d", (bool)wp_push_srv.response.success);
+   }
 
     ////////////////////////////////////////////
     /////////////////GUIDED/////////////////////
@@ -185,6 +156,21 @@ else
     // Subscribe to laser scanner data
     ros::Subscriber sub = n.subscribe<sensor_msgs::LaserScan>("/scan", 1000, scanCallback);
 
+    ////////////////////////////////////////////
+    ///////////////////LAND/////////////////////
+    ////////////////////////////////////////////
+    ros::ServiceClient land_cl = n.serviceClient<mavros_msgs::CommandTOL>("/mavros/cmd/land");
+    mavros_msgs::CommandTOL srv_land;
+    srv_land.request.altitude = 0;
+    srv_land.request.latitude = 0;
+    srv_land.request.longitude = 0;
+    srv_land.request.min_pitch = 0;
+    srv_land.request.yaw = 0;
+    if(land_cl.call(srv_land)){
+        ROS_INFO("srv_land send ok %d", srv_land.response.success);
+    }else{
+        ROS_ERROR("Failed Land");
+    }
 
     while (n.ok())
     {
