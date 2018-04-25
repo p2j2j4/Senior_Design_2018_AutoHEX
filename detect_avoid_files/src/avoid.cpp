9968934913149d72751fd18ec50ad0ed612a8c1d
Live session user <38jonesp@cua.edu>
2018-04-25 02:27:33 +0000

    Version 1.1 of the detect_avoid package
    Update includes:
    - bug fixes
    - angular velocity corrections for pitch and roll (throttle command still a mystery)
    - updates to syntax errors for C++
    - ros syntax updates for setting up publishers and subscribers
    - Guided mode set command moved  before waypoints are set (order subject to more changes)
    - Comments to make ROS easier to understand
    
    Version 1.1 will be built and tested ASAP

--- detect_avoid_files/src/avoid.cpp
+++ detect_avoid_files/src/avoid.cpp
@@ -2,10 +2,12 @@
 
 #include <ros/ros.h>
 
-//Mavros files
+// Mavros Commands
 #include <mavros_msgs/CommandBool.h>
 #include <mavros_msgs/CommandTOL.h>
 #include <mavros_msgs/SetMode.h>
+
+// Waypoints
 #include <mavros_msgs/WaypointPush.h>
 #include <mavros_msgs/WaypointClear.h>
 #include <mavros_msgs/CommandHome.h>
@@ -17,19 +19,37 @@ int main(int argc, char **argv){
     // Add subscriber for velocity commands published by detect.cpp
     int rate = 10;
 
+    // Initiate node
     ros::init(argc, argv, "detect_avoid");
+
     ros::NodeHandle p;
     ros::NodeHandle n;
     ros::NodeHandle l;
 
     ros::Rate r(rate);
+
+    ////////////////////////////////////////////
+    /////////////////GUIDED/////////////////////
+    ////////////////////////////////////////////
+    ros::ServiceClient cl = n.serviceClient<mavros_msgs::SetMode>("/mavros/set_mode");
+    mavros_msgs::SetMode srv_setMode;
+    srv_setMode.request.base_mode = 0;
+    srv_setMode.request.custom_mode = "GUIDED";
+
+    if(cl.call(srv_setMode)){
+        ROS_INFO("setmode send ok %d value:", srv_setMode.response.success);
+    }
+    else{
+        ROS_ERROR("Failed SetMode");
+        return -1;
+    }
     
     ////////////////////////////////////////////
     /////////////////Waypoints//////////////////
     ////////////////////////////////////////////
     ros::ServiceClient wp_clear_client = p.serviceClient<mavros_msgs::WaypointClear>("/mavros/mission/clear"); //("waypoint_clear_client")
     ros::ServiceClient wp_srv_client = n.serviceClient<mavros_msgs::WaypointPush>("waypoint_push_client");
-    ros::ServiceClient set_home_client = l.serviceClient<mavros_msgs::CommandHome>("/mavros/mission/home"); //("command_home_client")
+    ros::ServiceClient set_home_client = l.serviceClient<mavros_msgs::CommandHome>("command_home_client");
 
     mavros_msgs::WaypointPush wp_push_srv;
     mavros_msgs::WaypointClear wp_clear_srv;
@@ -49,8 +69,11 @@ int main(int argc, char **argv){
     wp_msg.x_lat = 40.0;
     wp_msg.y_long = 30.0;
     wp_msg.z_alt = 5.0;
-    
-   wp_push_srv.request.waypoints.push_back(wp_msg);
+
+    wp_clear_srv.request = {};
+
+    wp_push_srv.request.start_index = 0;
+    wp_push_srv.request.waypoints.push_back(wp_msg);
 
    set_home_srv.request.current_gps = false;
    set_home_srv.request.latitude = 11;
@@ -63,7 +86,6 @@ int main(int argc, char **argv){
    else{
      ROS_ERROR("Home position couldn't been changed");
    }
-
 
    if (wp_clear_client.call(wp_clear_srv)){
      ROS_INFO("Waypoint list was cleared");
@@ -79,22 +101,6 @@ int main(int argc, char **argv){
      ROS_ERROR("Waypoint couldn't been sent");
      ROS_INFO("Success:%d", (bool)wp_push_srv.response.success);
    }
-
-    ////////////////////////////////////////////
-    /////////////////GUIDED/////////////////////
-    ////////////////////////////////////////////
-    ros::ServiceClient cl = n.serviceClient<mavros_msgs::SetMode>("/mavros/set_mode");
-    mavros_msgs::SetMode srv_setMode;
-    srv_setMode.request.base_mode = 0;
-    srv_setMode.request.custom_mode = "GUIDED";
-
-    if(cl.call(srv_setMode)){
-        ROS_INFO("setmode send ok %d value:", srv_setMode.response.success);
-    }
-    else{
-        ROS_ERROR("Failed SetMode");
-        return -1;
-    }
 
     ////////////////////////////////////////////
     ///////////////////ARM//////////////////////
@@ -123,7 +129,6 @@ int main(int argc, char **argv){
     }else{
         ROS_ERROR("Failed Takeoff");
     }
-    
     
     ROS_INFO("Check");
 
--- detect_avoid_files/src/detect.cpp
+++ detect_avoid_files/src/detect.cpp
@@ -11,44 +11,44 @@ public:
     ScanAndDetect(){
 
         // Initialize Velocity command service
-        ros::Publisher vel_cmd = n.advertise<geometry_msgs::TwistStamped>("mavros/setpoint_velocity/cmd_vel", 100);
+        velocity = n.advertise<geometry_msgs::TwistStamped>("mavros/setpoint_velocity/cmd_vel", 100);
 
         // Initialize Subscriber
-        ros::Subscriber sub = n.subscribe<sensor_msgs::LaserScan>("/scan", 1000, &ScanAndDetect::scanCallback, this);
+        sub = n.subscribe<sensor_msgs::LaserScan>("/scan", 1000, &ScanAndDetect::scanCallback, this);
     }
 
 void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
 {
     //int count = scan->scan_time / scan->time_increment;
 
-    // Set up Variables
-    double ros_roll      = 0.0;
-    double ros_pitch     = 0.0;
+    // Set up 3D control Variables
+//  double ros_roll      = 0; // Exclude: will change on a case to case basis
+//  double ros_pitch     = -0.5;
     double ros_yaw       = 0.0;
     double ros_throttle  = 0.0;
 
-    n.param<double>("ros_roll", ros_roll, 0.0);
-    n.param<double>("ros_pitch", ros_pitch, 0.0);
-    n.param<double>("ros_yaw", ros_yaw, 0.0);
-    n.param<double>("ros_throttle", ros_throttle,0.0);
+//    n.param<double>("ros_roll", ros_roll, 0.0);
+//    n.param<double>("ros_pitch", ros_pitch, 0.0);
+//    n.param<double>("ros_yaw", ros_yaw, 0.0);
+//    n.param<double>("ros_throttle", ros_throttle,0.0);
 
-    // Figure out ranges for each for loop
-        for(int i = -115; -116 < i < 115; i++) {
+    // Objects that are within -115 and 115 degrees (in front of copter)
+        for(int i = -115; (-116 < i) && (i < 115); i++) {
             float degree = RAD2DEG(scan->angle_min + scan->angle_increment * i);
 
-            // Initialize Publisher
+            // Initialize Publisher variable: msg
             geometry_msgs::TwistStamped msg;
 
             if (scan->ranges[i] < .3){ //change to .5 for actual coding
                     msg.twist.linear.x = ros_throttle;
                     msg.twist.linear.y = 0;
                     msg.twist.linear.z = 0;
-                    msg.twist.angular.x = ros_pitch;
-                    msg.twist.angular.y = ros_roll;
-                    msg.twist.angular.z = ros_yaw;
-                    ROS_ERROR(": [%f,%f]", degree, scan->ranges[i]);
-                    vel_cmd.publish(msg); }
-
+                    msg.twist.angular.x = -5; //pitch backwards hard
+                    msg.twist.angular.y = 0;  //roll
+                    msg.twist.angular.z = ros_yaw;   //yaw
+                    ROS_ERROR(":DANGER object: [%f,%f]", degree, scan->ranges[i]);
+                    velocity.publish(msg); }
+         }
 // TO DO: figure out what the velocity values represent and implement them for diffferent object distances
 //            if (.5 < scan->ranges[i] < 3){ //between .5 and 3 meters
 //                    urgency = 2; // object is too close
@@ -64,20 +64,19 @@ void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
 //                   urgency = 0; // no objects in front
 //                  ROS_INFO(": [%f,%f]", degree, scan->ranges[i]);
 //                    detection_pub.publish(urgency); }
-       }
 }
 private:
     ros::NodeHandle n;
-    ros::Publisher vel_cmd;
+    ros::Publisher velocity;
     ros::Subscriber sub;
 
-} // end of class
+}; // end of class
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
+
     ros::init(argc, argv, "detect");
 
-    ScanAndDetect SAPObject;
+    ScanAndDetect SAPObject; //remove object if failure occurs
 
     ros::spin();
 
