59d03168ee66a24e2e38ebb3f3ebc29a257e7cf3
Live session user <38jonesp@cua.edu>
2018-04-25 21:38:59 -0400

    Version 1.3 release
    Updates include:
    - Landing request moved to detect.cpp
    - If object gets within .5 meters of the LIDAR the copter will land itself
    - Revised angles from the previous release
    - New angles have been tested (region directly infront of the copter)
    - Modified info and warn ranges
    - OCD code clean-up
    - code reorganized for clarity and speed
    - Comments edited and revised

--- detect_avoid_files/src/avoid.cpp
+++ detect_avoid_files/src/avoid.cpp
@@ -16,16 +16,12 @@
 
 int main(int argc, char **argv){
 
-    // Add subscriber for velocity commands published by detect.cpp
-    int rate = 10;
-
     // Initiate node
     ros::init(argc, argv, "detect_avoid");
 
-    ros::NodeHandle p;
     ros::NodeHandle n;
-    ros::NodeHandle l;
 
+    int rate = 10;
     ros::Rate r(rate);
 
     ////////////////////////////////////////////
@@ -37,24 +33,20 @@ int main(int argc, char **argv){
     srv_setMode.request.custom_mode = "GUIDED";
 
     if(cl.call(srv_setMode)){
-        ROS_INFO("setmode send ok %d value:", srv_setMode.response.success);
-    }
+        ROS_INFO("setmode send ok %d value:", srv_setMode.response.success); }
     else{
-        ROS_ERROR("Failed SetMode");
-        return -1;
-    }
+        ROS_WARN("Failed SetMode"); }
     
     ////////////////////////////////////////////
     /////////////////Waypoints//////////////////
     ////////////////////////////////////////////
-    ros::ServiceClient wp_clear_client = p.serviceClient<mavros_msgs::WaypointClear>("/mavros/mission/clear"); //("waypoint_clear_client")
+    ros::ServiceClient wp_clear_client = n.serviceClient<mavros_msgs::WaypointClear>("/mavros/mission/clear"); //("waypoint_clear_client")
     ros::ServiceClient wp_srv_client = n.serviceClient<mavros_msgs::WaypointPush>("waypoint_push_client");
-    ros::ServiceClient set_home_client = l.serviceClient<mavros_msgs::CommandHome>("command_home_client");
+    ros::ServiceClient set_home_client = n.serviceClient<mavros_msgs::CommandHome>("command_home_client");
 
     mavros_msgs::WaypointPush wp_push_srv;
     mavros_msgs::WaypointClear wp_clear_srv;
     mavros_msgs::CommandHome set_home_srv;
-
 
     mavros_msgs::Waypoint wp_msg;
 
@@ -70,37 +62,27 @@ int main(int argc, char **argv){
     wp_msg.y_long = 30.0;
     wp_msg.z_alt = 5.0;
 
-    wp_clear_srv.request = {};
-
-    wp_push_srv.request.start_index = 0;
     wp_push_srv.request.waypoints.push_back(wp_msg);
 
-   set_home_srv.request.current_gps = false;
-   set_home_srv.request.latitude = 11;
-   set_home_srv.request.longitude = 12;
-   set_home_srv.request.altitude = 1;
+    set_home_srv.request.current_gps = false;
+    set_home_srv.request.latitude = 11;
+    set_home_srv.request.longitude = 12;
+    set_home_srv.request.altitude = 1;
 
-   if (set_home_client.call(set_home_srv)){
-     ROS_INFO("Home was set to new value ");
-    }
-   else{
-     ROS_ERROR("Home position couldn't been changed");
-   }
+    if (set_home_client.call(set_home_srv)){
+      ROS_INFO("Home was set to new value "); }
+    else{
+      ROS_WARN("Home position couldn't been changed"); }
 
-   if (wp_clear_client.call(wp_clear_srv)){
-     ROS_INFO("Waypoint list was cleared");
-     }
-   else{
-     ROS_ERROR("Waypoint list couldn't been cleared");
-   }
+    if (wp_clear_client.call(wp_clear_srv)){
+      ROS_INFO("Waypoint list was cleared"); }
+    else{
+      ROS_WARN("Waypoint list couldn't been cleared"); }
 
-   if (wp_srv_client.call(wp_push_srv)){
-     ROS_INFO("Success:%d", (bool)wp_push_srv.response.success);
-   }
-   else{
-     ROS_ERROR("Waypoint couldn't been sent");
-     ROS_INFO("Success:%d", (bool)wp_push_srv.response.success);
-   }
+    if (wp_srv_client.call(wp_push_srv)){
+      ROS_INFO("Success:%d", (bool)wp_push_srv.response.success); }
+    else{
+      ROS_WARN("Waypoint couldn't been sent"); }
 
     ////////////////////////////////////////////
     ///////////////////ARM//////////////////////
@@ -109,10 +91,9 @@ int main(int argc, char **argv){
     mavros_msgs::CommandBool srv;
     srv.request.value = true;
     if(arming_cl.call(srv)){
-        ROS_INFO("ARM send ok %d", srv.response.success);
-    }else{
-        ROS_ERROR("Failed Arming - Check FCU pre-arms (again)");
-    }
+        ROS_INFO("ARM status: %d", srv.response.success); }
+    else{
+        ROS_WARN("Failed Arming - Check FCU pre-arms (again)"); }
 
     ////////////////////////////////////////////
     /////////////////TAKEOFF////////////////////
@@ -124,29 +105,11 @@ int main(int argc, char **argv){
     srv_takeoff.request.longitude = 0;
     srv_takeoff.request.min_pitch = 0;
     srv_takeoff.request.yaw = 0;
-    if(takeoff_cl.call(srv_takeoff)){
-        ROS_INFO("srv_takeoff send ok %d", srv_takeoff.response.success);
-    }else{
-        ROS_ERROR("Failed Takeoff");
-    }
-    
-    ROS_INFO("Check");
 
-    ////////////////////////////////////////////
-    ///////////////////LAND/////////////////////
-    ////////////////////////////////////////////
-    ros::ServiceClient land_cl = n.serviceClient<mavros_msgs::CommandTOL>("/mavros/cmd/land");
-    mavros_msgs::CommandTOL srv_land;
-    srv_land.request.altitude = 0;
-    srv_land.request.latitude = 0;
-    srv_land.request.longitude = 0;
-    srv_land.request.min_pitch = 0;
-    srv_land.request.yaw = 0;
-    if(land_cl.call(srv_land)){
-        ROS_INFO("srv_land send ok %d", srv_land.response.success);
-    }else{
-        ROS_ERROR("Failed Land");
-    }
+    if(takeoff_cl.call(srv_takeoff)){
+        ROS_INFO("Takeoff Status %d", srv_takeoff.response.success); }
+    else{
+        ROS_WARN("Failed Takeoff"); }
 
     while (n.ok())
     {
--- detect_avoid_files/src/detect.cpp
+++ detect_avoid_files/src/detect.cpp
@@ -14,39 +14,50 @@ public:
         velocity = n.advertise<geometry_msgs::TwistStamped>("mavros/setpoint_velocity/cmd_vel", 100);
 
         // Initialize Subscriber
-        sub = n.subscribe<sensor_msgs::LaserScan>("/scan", 1000, &ScanAndDetect::scanCallback, this);
+        sub = n.subscribe<sensor_msgs::LaserScan>("/scan", 400, &ScanAndDetect::scanCallback, this);
+
+        // Initialize Land Client
+        TOL = n.serviceClient<mavros_msgs::CommandTOL>("/mavros/cmd/land");
     }
 
 void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan) {
 
-    int count = scan->scan_time / scan->time_increment;
- // float start_ang = scan->angle_min;
+    // Initialize Publisher variable: msg
+       geometry_msgs::TwistStamped msg;
 
-    // Set up 3D control Variables
+    // Initialize Land service variable srv_land
+       mavros_msgs::CommandTOL srv_land;
+
+    int count = scan->scan_time / scan->time_increment;
+
+ // Set up velocity control variables
     double yaw       = 0.0;
     double throttle  = 2.0;
 
-    // Objects that are within -115 and 115 degrees (in front of copter)
-    for(int i = 0; count; i++) {
+    for(int i = 0; i < count; i++) { //Iterate over all scanned degrees
 
-     // Initialize Publisher variable: msg
-        geometry_msgs::TwistStamped msg;
+        float degree = RAD2DEG(scan->angle_increment * i);
+        bool land = false;
 
-     float degree = RAD2DEG(scan->angle_increment * i);
-
-          if ((degree > 115) && (degree < 245)) {
+          if ((degree < 20) || (degree > 320)) {
 
               if (scan->ranges[i] < .5){ // .5 meter buffer
-                      msg.twist.linear.x = throttle;
-                      msg.twist.linear.y = 0;
-                      msg.twist.linear.z = 0;
-                      msg.twist.angular.x = -5;  //pitch backwards hard
-                      msg.twist.angular.y = 0;   //roll
-                      msg.twist.angular.z = yaw; //yaw
-                      ROS_ERROR(":DANGER object: [%f,%f]", degree, scan->ranges[i]);
-                      velocity.publish(msg); }
 
-              if (scan->ranges[i] < 2.5){ // 2.5 meter buffer
+                      ROS_ERROR(":DANGER object: [%f,%f]", degree, scan->ranges[i]);
+
+                      if (!land) {
+                          land = true;
+                          srv_land.request.altitude = 0;
+                          srv_land.request.latitude = 0;
+                          srv_land.request.longitude = 0;
+                          srv_land.request.min_pitch = 0;
+                          srv_land.request.yaw = 0;
+                      if(TOL.call(srv_land)){
+                          ROS_INFO("LAND Status: %d", srv_land.response.success); }
+                      }
+              }
+
+              if ((scan->ranges[i] < 2) && (scan->ranges[i] >= .5)){ // 2 meter buffer
                       msg.twist.linear.x = throttle;
                       msg.twist.linear.y = 0;
                       msg.twist.linear.z = 0;
@@ -56,24 +67,25 @@ void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan) {
                       ROS_WARN(":Object at: [%f,%f] is nearing", degree, scan->ranges[i]);
                       velocity.publish(msg); }
 
-              if (scan->ranges[i] < 5){ //5 meter buffer
-                      msg.twist.linear.x = throttle -1; // slow down
+              if ((scan->ranges[i] < 4) && (scan->ranges[i] >= 2)){ //4 meter buffer
+                      msg.twist.linear.x = throttle -1; // slow down? TO DO: Learn what throttle values mean
                       msg.twist.linear.y = 0;
                       msg.twist.linear.z = 0;
-                      msg.twist.angular.x = 0;   //pitch backwards hard
-                      msg.twist.angular.y = 0;   //roll kinda hard
+                      msg.twist.angular.x = 0;   //pitch
+                      msg.twist.angular.y = 0;   //roll
                       msg.twist.angular.z = yaw; //yaw
                       ROS_INFO(":Object in range at: [%f] degrees at a distance of [%f]", degree, scan->ranges[i]);
                       velocity.publish(msg); }
 
-            } // end range test between 115 and 245 degrees
-         } // end scan itteration
+            } // end range test between 320 and 20 degrees
+          } // end scan itteration
 } // end laser scanner callback function
 
 private:
     ros::NodeHandle n;
     ros::Publisher velocity;
     ros::Subscriber sub;
+    ros::ServiceClient TOL;
 
 }; // end of class
 
